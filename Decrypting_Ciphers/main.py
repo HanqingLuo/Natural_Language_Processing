2f5b04fd76396ed51d2fb2c13f5f977033e8f7b9
Hanqing Luo <kukuman520@gmail.com>
2020-11-09 18:55:39 -0500

    Cipher

--- Decrypting_Ciphers/main.py
+++ Decrypting_Ciphers/main.py
@@ -12,3 +12,25 @@ import re
 import requests
 import os
 import textwrap
+
+### create substitution cipher
+
+# # one will act as the key, other as the value
+# # create a list of 26 letters
+# letters1 = list(string.ascii_lowercase)
+# letters2 = list(string.ascii_lowercase)
+# print('letters1 = ', letters1)
+# print('letters2 = ', letters2)
+
+
+# # create dictionay, key from letters1 maps value from letters2
+# true_mapping = {}
+
+# # shuffle second set of letters
+# random.shuffle(letters2)
+# print('random letters2 = ', letters2)
+
+# # populate map
+# for k, v in zip(letters1, letters2):
+#   true_mapping[k] = v
+# print('true_mapping = ', true_mapping)
